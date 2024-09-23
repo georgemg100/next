d4126107ddc8834106d28a5cd973d18ed25143ad
Michael Georgescu <michaelgeorgescu@macbook-pro.myfiosgateway.com>
2024-09-23 10:28:59 -0400

    update welcome message

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
             <div className="max-w-md mx-auto">
               <div>
-                <h1 className="text-2xl font-semibold">Welcome to NextAuth.js!</h1>
+                <h1 className="text-2xl font-semibold">Welcome to Anthill Coder!</h1>
               </div>
               <div className="divide-y divide-gray-200">
                 <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
